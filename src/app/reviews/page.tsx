b1635c3e429851eaf40b3c702d70bf7e9ae4cb1a
Jennifer Weaver <jennifer.weaver.0622@gmail.com>
2026-03-09 20:41:20 -0500

    Add reviews page

---
+++ src/app/reviews/page.tsx
@@ -0,0 +1,9 @@
+import ReviewsSection from "@/components/ReviewsSection";
+
+export default function ReviewsPage() {
+  return (
+    <div className="bg-slate-950 min-h-screen">
+      <ReviewsSection />
+    </div>
+  );
+}
